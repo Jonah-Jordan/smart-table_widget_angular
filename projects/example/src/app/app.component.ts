cbb66af1cd85272d32430008b9be8ce978158f1d
DESKTOP-CEUJU05\Nicolas <nicolas.keirsmaekers@digipolis.be>
2022-06-24 15:03:52 +0200

    FIX: formatted

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from "@angular/core";
-import { AppRatingComponent } from "./rating.component";
-import { SmartTableConfig } from "projects/smart-table/src/lib/smart-table/smart-table.types";
+import { Component } from '@angular/core';
+import { AppRatingComponent } from './rating.component';
+import { SmartTableConfig } from 'projects/smart-table/src/lib/smart-table/smart-table.types';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "example";
+  refresh = false;
+  title = 'example';
 
   moviesCustomColumns = [
     {
-      name: "rating",
+      name: 'rating',
       component: AppRatingComponent,
     },
   ];
@@ -20,20 +21,20 @@ export class AppComponent {
   customConfiguration: SmartTableConfig = {
     options: {
       defaultSortOrder: {
-        key: "title_year",
-        order: "desc",
+        key: 'title_year',
+        order: 'desc',
       },
       persistTableConfig: true,
-      storageIdentifier: "test-smart-table",
+      storageIdentifier: 'test-smart-table',
       translations: {
-        moreFilters: "Extra filters",
-        export: "Exporteer",
-        apply: "Toepassen",
+        moreFilters: 'Extra filters',
+        export: 'Exporteer',
+        apply: 'Toepassen',
       },
     },
   };
 
   onRowClicked(row) {
-    console.log("clicked row", row);
+    console.log('clicked row', row);
   }
 }
